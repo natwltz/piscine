d6e5c6ac134e48cb42b61268f5253d872f3b3d09
natwltz <nathan.waltzer@stagiairesmns.fr>
2024-11-25 10:27:49 +0100

    modif code

--- exoPiscine.py
+++ exoPiscine.py
@@ -1,6 +1,7 @@
 liste = [("Nathan","Crawl",20),("Jacque","Dos",10),("Paul","Brasse",4)]
 cmd = ""
-while cmd != "exit":
+exit = True
+while exit:
     cmd = input("que faut t-il faire ?(ajout,liste ou exit)")
     if cmd == "ajout":
         a = input("qui? :")
@@ -8,8 +9,14 @@ while cmd != "exit":
         c = input("combien de longueur ? :")
         t = (a,b,c)
         liste.append(t)
+        continue
     if cmd == "liste":
         for elt in liste:
             print("nom :",elt[0])
             print("nage : ",elt[1])
             print("longueur : ",elt[2])
+            continue
+    if cmd == "exit":
+        exit = False
+        continue
+    print(f"Commande {cmd} inconnue")
